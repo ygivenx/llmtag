8d4ae0213426823769b38235ccd185404c3d1271
PorwalS <PorwalS@mskcc.org>
2023-10-12 14:24:31 -0400

    Added function to call sim data in main package, called get_note_label function in __init__.py to make it user-facing

--- llmtag/__init__.py
+++ llmtag/__init__.py
@@ -1,1 +1,17 @@
 __version__ = '0.1.0'
+
+"""
+Labeling txt in python
+
+Modules exported by this package:
+
+- `get_note_label`
+- `load_test_data`
+
+"""
+
+from llmtag import load_test_data
+from llmtag.llm_label import get_note_label
+import os
+
+data = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
