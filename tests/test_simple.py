bf13a1f23cd5be33148a7429ac1a80fcaad5a3fb
PorwalS <PorwalS@mskcc.org>
2023-10-18 12:01:48 -0400

    Remove unnecessary data

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -16,18 +16,14 @@ def test_load_simdata1():
 def test_load_labeled_simdata1():
     print('\n', load_labeled_df1())
 
-# def test_llm_simple_prompt(note):
-#     res = get_note_label(note, n_ctx=512)
-#     assert int(res["label"]) == 1
+def test_llm_simple_prompt(note):
+    res = get_note_label(note, n_ctx=512)
+    assert int(res["label"]) == 1
 
 def test_multiple_notes(notes):
 
     counter = 0
     for note, label in notes:
-        counter+=1
-        print(counter)
-        if counter<10:
-            continue
         res = get_note_label(note, n_ctx=512)
         if res['label'] == 'NA':
             assert label == 0
