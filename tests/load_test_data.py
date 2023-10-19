a15948bf0fa3b8a7e1f9a7cae46ade54028a8688
PorwalS <PorwalS@mskcc.org>
2023-10-19 19:21:42 -0400

    Update testing

--- tests/load_test_data.py
+++ tests/load_test_data.py
@@ -16,3 +16,30 @@ def load_data_file(file_type):
     file_path = os.path.join(os.path.dirname(__file__), f'benchmark_data/{filename}')
     with open(file_path, 'r') as f:
         return pd.read_csv(f, encoding='latin-1')
+<<<<<<< HEAD
+=======
+
+def load_simulation_df1():
+    '''
+    :return pd.DataFrame that contains sim data
+    '''
+    return load_data_file('df_simulation1.csv')
+
+def load_gpt_sim():
+    '''
+    :return pd.DataFrame that contains gpt sim data
+    '''
+    return load_data_file('gpt_sim.csv')
+
+def load_gpt_sim_small():
+    '''
+    :return pd.DataFrame that contains gpt sim small data
+    '''
+    return load_data_file('gpt_sim_small.csv')
+
+def load_labeled_df1():
+    '''
+    Load LLM-labeled simulation data
+    '''
+    return load_data_file('out.csv')
+>>>>>>> fe4a403 (Condense loading data to single function and make changes in tests, rebase on Rohans changes)
