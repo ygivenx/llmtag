592073a7aed8940e82608512756ad3e1c31405d5
PorwalS <PorwalS@mskcc.org>
2023-10-19 19:13:53 -0400

    Call all test data using single function, change in test scripts as well

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from .load_test_data import load_simulation_df1
+from .load_test_data import load_data_file
 
 one_note = """
             FINDINGS:
@@ -18,7 +18,7 @@ def note():
 
 @pytest.fixture
 def notes():
-    data = load_simulation_df1().sample(10, random_state=42)
+    data = load_data_file('df_simulation1.csv').sample(10, random_state=42)
     labels = data.label.tolist()
     notes = data.notes.tolist()
     yield zip(notes, labels)
