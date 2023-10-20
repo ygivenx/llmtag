d9d9df205c56c95fc554028769aab9679d5639b0
PorwalS <PorwalS@mskcc.org>
2023-10-19 20:47:04 -0400

    Remove unit test that has to do with path to model

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -3,12 +3,6 @@ import pandas as pd
 
 from .load_test_data import load_data_file  # Import the new function
 
-model_path = os.path.join(os.getcwd(), 'models/7B/llama-2-7b-chat.Q4_K_M.gguf')
-
-def test_model_path_exists():
-    print(model_path)
-    assert os.path.exists(model_path)
-
 def test_load_simdata1():
     df = load_data_file('df_simulation1.csv')  # Use the new function with the appropriate argument
     assert isinstance(df, pd.DataFrame)  # Check if it's a DataFrame
